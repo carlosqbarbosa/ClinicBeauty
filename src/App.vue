76f129eb0121d46a7fef6472776a495bccca84b2
Carlosbarbosa <carloshenrique29.qb@gmail.com>
2025-06-30 18:33:08 -0300

    feito a parte de serviços

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <TheHeader/>
     <Hero />
+    <Services />
   </div>
 </template>
 <script setup>
@@ -9,6 +10,7 @@
 import {onMounted} from 'vue';
 import TheHeader from './components/TheHeader.vue';
 import Hero from './components/Hero.vue';
+import Services from './components/Services.vue';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 onMounted(() => {
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -16,7 +16,8 @@
             </p>
           </hgroup> 
           <!--CTA buttons--> 
-          <nav data-aos="fade-up" data-aos-delay="400" 
+          <nav data-aos="fade-up" 
+          data-aos-delay="600" 
             class="flex flex-col sm:flex-row justify-center lg:justify-start gap-4 mb-12">
                 <a href="#appointment" 
                   class="bg-pink-600 hover:bg-pink-700 text-white text-center px-8 py-3 rounded-full 
@@ -28,7 +29,7 @@
                 text-center px-8 py-3 rounded-full transition" aria-label="Explore our Services">Explore Services</a>
             </nav>
             <!--Status Section-->
-            <aside data-aos="fade-up" data-aos-delay="600" class="py-4" >
+            <aside data-aos="fade-up" data-aos-delay="800" class="py-4" >
               <ul class="flex flex-wrap justify-center lg:justify-start gap-x-6 gap-y-4 text-center">
                 <li v-for="(start, index) in stats" :key="index" class="w-1/2 sm:w-auto">
                   <strong class="text-2x1 font-bold text-pink-600">{{ stats.value }}</strong>
@@ -41,8 +42,10 @@
         <!--Hero Image-->
         <figure data-aos="fade-up" data-aos-delay="700" class="w-full lg:w-1/2 flex justify-center relative">
           <div class="relative w-full max-w-xs sm:max-sm md:max-w-md">
-            <span class="bg-pink-500 rounded-full w-64 g-64 sm:w-80 sm:h-80 absolute -top-10 -letf-10
+            <span class="bg-pink-500 rounded-full w-64 h-64 sm:w-80 sm:h-80 absolute -top-10 -letf-10
             opacity-20" aria-hidden="true"></span>
+            <span class="bg-purple-400 rounded-full w-48 h-48 sm:w-64 sm:h-64 absolute -bottom-10 -right-10
+            opacity-20"></span>
            <img :src="imageSrc" alt="Woman smiling after cosmetic treatment" 
             class="relative z-10 rounded-full shadow-2x1 w-full object-cover" 
             width="400" height="400" loading="eager"/>
