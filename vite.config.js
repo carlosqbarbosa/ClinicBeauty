c9cba8c356760f73549b574abc63b4baf534360c
Carlosbarbosa <carloshenrique29.qb@gmail.com>
2025-11-06 17:06:52 -0300

    update vite

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
+const isVercel = process.env.VERCEL === '1'
+
 export default defineConfig({
-   base: '/ClinicBeauty/',
+  base: isVercel ? '/' : '/ClinicBeauty/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)), // <- ESSENCIAL
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
       '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
     }
   },
 })
-
